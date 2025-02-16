62c0a5b82e10ee70fe3e874a2c0fa76322dc8370
burce <burcev-egor@mail.ru>
2025-02-16 13:16:01 +0500

    graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,14 @@ import (
 	"MerchShop/internal/application/core/tokens"
 	"MerchShop/internal/config"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	if config.GetEnv() == "development" {
 		log.SetLevel(log.DebugLevel)
 	}
@@ -19,9 +24,17 @@ func main() {
 		log.Fatal(err)
 	}
 	app := api.NewApplication(dbAdapter, tokenHandler)
-	rtr := router.NewRouter(app, config.GetApplicationPort())
-	err = rtr.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
+	port := config.GetApplicationPort()
+	rtr := router.NewRouter(app, port)
+	go func() {
+		log.Printf("Server is starting on :%s...", port)
+		err := rtr.Start()
+		if err != nil {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+	<-stop
+	log.Println("closing database connection...")
+	dbAdapter.Close()
+	log.Println("finished")
 }
